ae1bae2298c61af24a9e42a7de1cd1d1735d7090
Gareth <me@gareth.codes>
2019-08-26 05:39:22 +0000

    meh

--- blink.c
+++ blink.c
@@ -1,29 +1,20 @@
 #define F_CPU 1000000L
 #include <avr/io.h>
+#include <util/twi.h>
 #include <util/delay.h>
+#include "i2cmaster.S"
+
+#define address 0x50
 
 int main (void)
-{
-  // set PB3 to be output
-    DDRB = 0b00001000;
-  while (1) {
-    
-    // flash# 1:
-    // set PB3 high
-    PORTB = 0b00001000; 
-    _delay_ms(1000);
-    // set PB3 low
-    PORTB = 0b00000000;
-    _delay_ms(2000);
+{	
 
-    // flash# 2:
-    // set PB3 high
-    PORTB = 0b00001000; 
-    _delay_ms(3000);
-    // set PB3 low
-    PORTB = 0b00000000;
-    _delay_ms(1000);
-  }
- 
-  return 1;
+ 	i2c_init();
+// 	i2c_start_wait(address+I2C_WRITE);
+// 	i2c_write(0x00);
+// 	i2c_write(0x00);
+// 	i2c_write(0x42);
+// 	i2c_stop();
+
+	return 1;
 }
